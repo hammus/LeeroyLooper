589d1dd4fa4b24b10dae156cc81c5aa3bdabc505
Liam Whan <liam.whan@gmail.com>
2017-09-27 22:34:26 +1000

    add findDevice

--- main.cpp
+++ main.cpp
@@ -3,7 +3,10 @@
 int main() {
 
     Looper looper;
+
+    looper.printDeviceList();
     int MyDevice = looper.findDevice("Focusrite");
+    std::cout << "Device Result: " << MyDevice << std::endl;
     return 0;
    
 }
