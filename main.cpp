15193d06ad5000607b6e0c9debd675f4e4d2577e
Liam Whan <liam.whan@gmail.com>
2017-09-27 22:37:57 +1000

    Test findDevice

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
     Looper looper;
 
     looper.printDeviceList();
-    int MyDevice = looper.findDevice("Focusrite");
+    int MyDevice = looper.findDevice("Scarlett");
     std::cout << "Device Result: " << MyDevice << std::endl;
     return 0;
    
